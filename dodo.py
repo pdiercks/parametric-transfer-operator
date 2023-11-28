67f22784042ef5557b25643169ff6ad1af7f1c29
Philipp Diercks <philipp.diercks@bam.de>
2023-11-28 10:45:33 +0100

    modular approach for dodo.py

--- dodo.py
+++ dodo.py
@@ -1,10 +1,6 @@
-def task_fom():
-    # instantiate example to define targets
-    from src.getting_started.definitions import Example
-    ex = Example(name="beam")
-    return {
-            "file_dep": ["./src/getting_started/fom.py"],
-            "actions": ["python3 %(dependencies)s"],
-            "targets": [ex.fine_grid],
-            "clean": True,
-            }
+"""main workflow definition"""
+
+# TODO add high level tasks
+
+# import tasks for individual examples
+from src.getting_started.tasks import *
