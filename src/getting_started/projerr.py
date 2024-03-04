a7144ad104d5f45cd7b03cda30bffef37e6f0c11
Philipp Diercks <philipp.diercks@bam.de>
2024-03-04 17:49:18 +0100

    adjust plots for projection error & svals

--- src/getting_started/projerr.py
+++ src/getting_started/projerr.py
@@ -2,20 +2,19 @@ from pathlib import Path
 
 from mpi4py import MPI
 from basix.ufl import element
-from dolfinx import fem
-from dolfinx.fem.petsc import set_bc
+from dolfinx import mesh, fem, default_scalar_type
 from dolfinx.io import gmshio
 import numpy as np
 
-from multi.domain import RectangularDomain
-from multi.projection import compute_absolute_proj_errors
+from multi.domain import RectangularSubdomain
+from multi.projection import compute_relative_proj_errors
 from multi.product import InnerProduct
-from multi.bcs import BoundaryConditions
-from multi.solver import build_nullspace
+from multi.materials import LinearElasticMaterial
+from multi.problems import LinElaSubProblem
+from multi.shapes import NumpyLine
 
 from pymor.core.logger import getLogger
 from pymor.core.defaults import set_defaults
-from pymor.algorithms.gram_schmidt import gram_schmidt
 from pymor.bindings.fenicsx import FenicsxVectorSpace, FenicsxMatrixOperator
 
 
@@ -37,14 +36,11 @@ def main(args):
     domain, _, _ = gmshio.read_from_msh(
         meshfile.as_posix(), MPI.COMM_SELF, gdim=beam.gdim
     )
-    omega = RectangularDomain(domain)
+    omega = RectangularSubdomain(99, domain)
 
     # ### Beam Problem definitions
     beamproblem = BeamProblem(beam.coarse_grid.as_posix(), beam.fine_grid.as_posix())
     cell_index = beamproblem.config_to_cell(args.configuration)
-    dirichlet = beamproblem.get_dirichlet(cell_index)
-    kernel_set = beamproblem.get_kernel_set(cell_index)
-    logger.debug(f"{kernel_set=}")
 
     # ### Translate the unit cell domain
     unit_length = omega.xmax[0] - omega.xmin[0]
@@ -52,44 +48,74 @@ def main(args):
     dx = np.array([[deltax, 0.0, 0.0]])
     omega.translate(dx)
 
+    omega.create_coarse_grid(1)
+    omega.create_boundary_grids()
+
     fe = element("P", domain.basix_cell(), beam.fe_deg, shape=(beam.gdim,))
     V = fem.functionspace(domain, fe)
-    source = FenicsxVectorSpace(V)
 
-    # ### Range product operator
-    # get homogeneous Dirichlet bcs if present
-    bchandler = BoundaryConditions(domain, V)
-    bc_hom = []
-    if dirichlet is not None:
-        bchandler.add_dirichlet_bc(**dirichlet)
-        bc_hom = bchandler.bcs
+    phases = LinearElasticMaterial(2, 20e3, 0.3) # material will not be important here
+    problem = LinElaSubProblem(omega, V, phases=(phases,))
+    problem.setup_edge_spaces()
+    problem.create_map_from_V_to_L()
 
-    inner_product = InnerProduct(V, beam.range_product, bcs=bc_hom)
-    pmat = inner_product.assemble_matrix()
-    product = FenicsxMatrixOperator(pmat, V, V)
+    # ### Load test data
+    # test data contains full displacement solution restricted to edges
+    npz_testset = np.load(beam.fom_test_set(args.configuration))
+    npz_basis = np.load(beam.fine_scale_edge_modes_npz(args.distribution, args.configuration))
+    errors = {}
 
-    # ### Rigid body modes
-    gdim = beam.gdim
-    ns_vecs = build_nullspace(V, gdim=gdim)
-    rigid_body_modes = []
-    for j in kernel_set:
-        set_bc(ns_vecs[j], bc_hom)
-        rigid_body_modes.append(ns_vecs[j])
-    basis = source.make_array(rigid_body_modes)
+    for edge in ["bottom", "left", "right", "top"]:
+        edge_space = problem.edge_spaces["fine"][edge]
 
-    basis_vectors = np.load(beam.loc_pod_modes(args.distribution, args.configuration))
-    B = source.from_numpy(basis_vectors)
-    basis.append(B)
+        # BCs for range product
+        facet_dim = edge_space.mesh.topology.dim - 1
+        vertices = mesh.locate_entities_boundary(
+                edge_space.mesh, facet_dim, lambda x: np.full(x[0].shape, True, dtype=bool)
+                )
+        _dofs = fem.locate_dofs_topological(edge_space, facet_dim, vertices)
+        gdim = edge_space.mesh.geometry.dim
+        bc_hom = fem.dirichletbc(
+            np.array((0,) * gdim, dtype=default_scalar_type), _dofs, edge_space
+        )
 
-    gram_schmidt(basis, product=product, copy=False)
+        # range product
+        range_product = InnerProduct(edge_space, beam.range_product, bcs=[bc_hom])
+        product_matrix = range_product.assemble_matrix()
+        product = FenicsxMatrixOperator(product_matrix, edge_space, edge_space)
 
-    # ### load test data
-    data = np.load(beam.fom_test_set(args.configuration))
-    U = source.from_numpy(data)
+        # fine scale edge basis
+        source = FenicsxVectorSpace(edge_space)
+        basis = source.from_numpy(npz_basis[edge])
+        G = basis.gramian(product)
+        orthonormal = np.isclose(np.sum(G), len(basis))
+        assert orthonormal
+
+        # test data
+        U = source.from_numpy(npz_testset[edge])
+
+        # subtract coarse scale part
+        if edge in ("bottom", "top"):
+            component = 0
+        else:
+            component = 1
+        xmin = np.amin(edge_space.mesh.geometry.x, axis=0)
+        xmax = np.amax(edge_space.mesh.geometry.x, axis=0)
+        nodes = np.array([xmin[component], xmax[component]])
+        line = NumpyLine(nodes)
+        shapes = line.interpolate(edge_space, component)
+        coarse_basis = source.from_numpy(shapes)
+        assert len(coarse_basis) == 2 * gdim
+        dofs = bc_hom._cpp_object.dof_indices()[0]
+        u_dofs = U.dofs(dofs)
+        U -= coarse_basis.lincomb(u_dofs)
+
+        # compute projection error for fine scale part
+        errs = compute_relative_proj_errors(U, basis, product=product, orthonormal=orthonormal)
+        errors[edge] = errs
 
     # TODO maybe have both absolute and relative errors written to disk?
-    errs = compute_absolute_proj_errors(U, basis, product=product, orthonormal=True)
-    np.save(beam.proj_error(args.distribution, args.configuration), errs)
+    np.savez(beam.proj_error(args.distribution, args.configuration), **errors)
 
 
 if __name__ == "__main__":
