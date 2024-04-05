7d6819f7ef346162d2e3b0d5dfcd0807273cf1c1
Philipp Diercks <philipp.diercks@bam.de>
2024-04-05 17:49:26 +0200

    ei of transformation operator

--- src/parageom/ei_map.py
+++ src/parageom/ei_map.py
@@ -1,16 +1,19 @@
 """empirical interpolation of the transformation operator"""
 
+from typing import Union
+
 from dolfinx import fem
 from pymor.parameters.base import ParameterSpace
 import basix
 import ufl
 
+import numpy as np
+from scipy.linalg import solve_triangular
 from pymor.bindings.fenicsx import FenicsxVectorSpace
 from pymor.algorithms.ei import ei_greedy
 
 
-def main():
-    from .tasks import example
+def interpolate_transformation_operator(example, test: bool = True, output: Union[str, None] = None):
     from .auxiliary_problem import discretize_auxiliary_problem
 
     # ### Auxiliary problem
@@ -20,7 +23,7 @@ def main():
     # ### Define training set (Parameters({"R": 1}))
     mu_range = example.mu_range
     parameter_space = ParameterSpace(param, mu_range)
-    training_set = parameter_space.sample_randomly(10)
+    training_set = parameter_space.sample_randomly(100)
 
     # transformation displacement
     d = fem.Function(aux.problem.V, name="d")
@@ -56,28 +59,74 @@ def main():
     source = FenicsxVectorSpace(Q)
     snap_vecs = []
 
+    print("Computing snapshots of transformation operator for training set of"
+          f" size {len(training_set)}.")
     for mu in training_set:
         aux.solve(d, mu)
         d.x.scatter_forward()
         op_fun.interpolate(op_expr)
+        op_fun.x.scatter_forward()
         snap_vecs.append(op_fun.vector.copy())
 
     snapshots = source.make_array(snap_vecs)
+
     # TODO use proper norm
+    # operator: FenicsxMatrixOperator
+    # norm = pymor.constructions.induced_norm(operator)
+    # FIXME 
+    # FeniscxMatrixOperator.matrix would need to have size (dim_qspace, dim_qspace)
+
+    # Is Euclidean norm actually not problematic, because I am measuring an element of
+    # a quadrature space?
+
     error_norm = None # uses euclidean norm
     atol = 1e-3
     rtol = None
     interp_dofs, collateral_basis, ei_data = ei_greedy(snapshots, error_norm=error_norm, atol=atol, rtol=rtol, copy=True)
 
     print(f"Number of basis functions: {len(collateral_basis)}")
-    # for atol=1e-3, I get 7 basis functions
 
-    # TODO output
-    # interp_dofs
-    # collaterial_basis
-    # ei_data['errors']
-    # ei_data['triangularity_errors']
+    # make test
+    if test:
+        interpolation_matrix = collateral_basis.dofs(interp_dofs).T
+        validation_set = parameter_space.sample_randomly(20)
+        errors = []
+        for mu in validation_set:
+            # fom
+            aux.solve(d, mu)
+            d.x.scatter_forward()
+            op_fun.interpolate(op_expr)
+            op_fun.x.scatter_forward()
+
+            # reduced representation
+            # FIXME restricted evaluation is not implemented
+            # therefore using full operator here for AU
+            AU = op_fun.x.array[interp_dofs]
+            coeff = solve_triangular(interpolation_matrix, AU, lower=True, unit_diagonal=True).T
+            op_ei = collateral_basis.lincomb(coeff)
+            
+            # error
+            err = op_fun.x.array - op_ei.to_numpy().flatten()
+            errors.append(np.linalg.norm(err))
+
+        print(f"Max. error {np.amax(errors)} over validation set of size {len(validation_set)}.")
+
+    # TODO: restricted evaluation
+    # use fem.Expression.eval instead of op_fun.interpolate
+    # fem.Expression.eval requires cells
+    # need to determine cells that contain interp_dofs
+    # figure out how to filter out qpoints that are not interp_dofs
+
+    if output is not None:
+        print("output not implemented")
+        # TODO output
+        # interp_dofs
+        # collaterial_basis
+        # ei_data['errors']
+        # ei_data['triangularity_errors']
 
 
 if __name__ == "__main__":
-    main()
+    from .tasks import example
+    interpolate_transformation_operator(example, test=True, output=None)
+
