4d49f23282bb9a84db3571a17e449a4a8339e826
Philipp Diercks <philipp.diercks@bam.de>
2024-04-22 18:45:53 +0200

    preprocessing: add create_parent_domain; format

--- src/parageom/preprocessing.py
+++ src/parageom/preprocessing.py
@@ -1,9 +1,11 @@
 import typing
 
-UNIT_LENGTH = 1.
+UNIT_LENGTH = 1.0
 
 
-def discretize_unit_cell(mu, num_cells: int, output: str, gmsh_options: typing.Optional[dict] = None) -> None:
+def discretize_unit_cell(
+    mu, num_cells: int, output: str, gmsh_options: typing.Optional[dict] = None
+) -> None:
     """Discretizes square domain with circular void.
 
     Args:
@@ -42,26 +44,93 @@ def create_structured_coarse_grid(config: str, output: str):
     match config:
         case "left":
             num_cells = (2, 1)
-            xmin = 0.
+            xmin = 0.0
         case "right":
             num_cells = (2, 1)
-            xmin = 8.
+            xmin = 8.0
         case "inner":
             num_cells = (3, 1)
-            xmin = 3.
+            xmin = 3.0
         case "global":
             num_cells = (example.nx, example.ny)
-            xmin = 0.
+            xmin = 0.0
         case _:
             raise NotImplementedError
 
     xmax = xmin + UNIT_LENGTH * num_cells[0]
-    ymin = 0.
+    ymin = 0.0
     ymax = ymin + UNIT_LENGTH * num_cells[1]
-    create_rectangle(xmin, xmax, ymin, ymax, num_cells=num_cells, recombine=True, out_file=output)
+    create_rectangle(
+        xmin, xmax, ymin, ymax, num_cells=num_cells, recombine=True, out_file=output
+    )
 
 
-def discretize_oversampling_domains(config: str):
+def create_parent_domain(config: str, output: str):
+    import tempfile
+
+    from mpi4py import MPI
+    from dolfinx.io import gmshio
+
+    from multi.domain import StructuredQuadGrid
+    from multi.preprocessing import create_voided_rectangle, merge_mshfiles
+
+    from .tasks import example
+
+    coarse_grid_msh = example.coarse_grid(config).as_posix()
+    coarse_domain = gmshio.read_from_msh(coarse_grid_msh, MPI.COMM_WORLD, gdim=2)[0]
+    coarse_grid = StructuredQuadGrid(coarse_domain)
+    num_cells = coarse_grid.num_cells
+
+    subdomains = []
+    to_be_merged = []
+    facet_tags = [
+        {"void": 15},
+        {"void": 16},
+        {"void": 17},
+        {"void": 18},
+        {"void": 19},
+        {"void": 20},
+        {"void": 21},
+        {"void": 22},
+        {"void": 23},
+        {"void": 24},
+    ]
+
+    offset = {2: 0, 1: 0}
+    for cell in range(num_cells):
+        subdomains.append(tempfile.NamedTemporaryFile(suffix=".msh"))
+        to_be_merged.append(subdomains[cell].name)
+        gmsh_options = {"Mesh.ElementOrder": example.geom_deg}
+
+        cell_vertices = coarse_grid.get_entities(0, cell)
+        lower_left = cell_vertices[:1]
+        xc = coarse_grid.get_entity_coordinates(0, lower_left)
+        xmin, ymin, zc = xc[0]
+        xmax = xmin + UNIT_LENGTH
+        ymax = ymin + UNIT_LENGTH
+
+        create_voided_rectangle(
+            xmin,
+            xmax,
+            ymin,
+            ymax,
+            z=zc,
+            radius=example.mu_bar,
+            num_cells=example.num_intervals,
+            recombine=True,
+            cell_tags={"matrix": 1},
+            facet_tags=facet_tags[cell],
+            out_file=to_be_merged[cell],
+            options=gmsh_options,
+            tag_counter=offset,
+        )
+
+    merge_mshfiles(to_be_merged, output)
+    for tmp in subdomains:
+        tmp.close()
+
+
+def create_physical_domain(config: str):
     import pathlib
     import tempfile
 
@@ -83,7 +152,9 @@ def discretize_oversampling_domains(config: str):
     training_set = training_data["training_set"]
 
     reference_cell = example.parent_unit_cell.as_posix()
-    aux = discretize_auxiliary_problem(reference_cell, example.geom_deg, example.parameters["subdomain"])
+    aux = discretize_auxiliary_problem(
+        reference_cell, example.geom_deg, example.parameters["subdomain"]
+    )
     d = fem.Function(aux.problem.V, name="transformation displacement")
 
     coarse_grid_msh = example.coarse_grid(config).as_posix()
@@ -91,12 +162,12 @@ def discretize_oversampling_domains(config: str):
     coarse_grid = StructuredQuadGrid(coarse_domain)
 
     for k, mu in enumerate(training_set):
-        subdomains = [] # list of msh files
+        subdomains = []  # list of msh files
 
         for mu_i in mu.to_numpy():
             # read reference mesh
             local_mu = aux.parameters.parse([mu_i])
-            d.x.array[:] = 0.
+            d.x.array[:] = 0.0
             aux.solve(d, local_mu)
             d.x.scatter_forward()
 
@@ -105,10 +176,13 @@ def discretize_oversampling_domains(config: str):
 
             # read parent unit cell via gmshio
             # (cannot use meshio because of dof layout, cell ordering)
-            parent_subdomain = gmshio.read_from_msh(reference_cell, MPI.COMM_WORLD, gdim=2)[0]
+            parent_subdomain, _, _ = gmshio.read_from_msh(
+                reference_cell, MPI.COMM_WORLD, gdim=2
+            )
             x_subdomain = parent_subdomain.geometry.x
-            disp = np.pad(d.x.array.reshape(x_subdomain.shape[0], -1),
-                          pad_width=[(0, 0), (0, 1)])
+            disp = np.pad(
+                d.x.array.reshape(x_subdomain.shape[0], -1), pad_width=[(0, 0), (0, 1)]
+            )
             x_subdomain += disp
 
             # write physical subdomain mesh to XDMF
@@ -117,15 +191,19 @@ def discretize_oversampling_domains(config: str):
             xdmf.write_mesh(parent_subdomain)
             xdmf.close()
 
+            # FIXME
+            # Writing facet tags has issues.
+            # The XDMFFile will not be readable by meshio. (XDMF Reader only supports one grid)
+
             subdomains.append(tmp_xdmf.name)
-
-
 
         oversampling_msh = example.oversampling_domain(config, k).as_posix()
         coarse_grid.fine_grid_method = subdomains
         coarse_grid.create_fine_grid(
-                np.arange(len(subdomains)), output=oversampling_msh, cell_type="quad"
-                )
+            np.arange(len(subdomains)),
+            output=oversampling_msh,
+            cell_type=example.cell_type,
+        )
 
         # delete tmp files
         for tmp in subdomains:
@@ -133,12 +211,24 @@ def discretize_oversampling_domains(config: str):
         subdomains.clear()
 
 
-
 if __name__ == "__main__":
     import sys, argparse
-    parser = argparse.ArgumentParser(description="Preprocessing for the parageom example.")
-    parser.add_argument("config", type=str, choices=("left", "inner", "right", "global"), help="The configuration of the oversampling problem.")
-    parser.add_argument("type", type=str, choices=("coarse", "oversampling"), help="Type of meshes that should be generated.")
+
+    parser = argparse.ArgumentParser(
+        description="Preprocessing for the parageom example."
+    )
+    parser.add_argument(
+        "config",
+        type=str,
+        choices=("left", "inner", "right", "global"),
+        help="The configuration of the domain.",
+    )
+    parser.add_argument(
+        "type",
+        type=str,
+        choices=("coarse", "oversampling", "parent"),
+        help="Type of meshes that should be generated.",
+    )
     parser.add_argument("--output", type=str, help="Write mesh to path if applicable.")
     args = parser.parse_args(sys.argv[1:])
 
@@ -150,6 +240,8 @@ if __name__ == "__main__":
         case "oversampling":
             # generate fine grid meshes of oversampling domains
             # TODO creation of meshes for oversampling
-            discretize_oversampling_domains(args.config)
+            create_physical_domain(args.config)
+        case "parent":
+            create_parent_domain(args.config, args.output)
 
     # TODO creation of global meshes for FOM (ROM validation)
