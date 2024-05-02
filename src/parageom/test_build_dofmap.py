49b3e1c8641d4a70aa581a4c82cb959c85a5e55c
Philipp Diercks <philipp.diercks@gmail.com>
2024-05-02 18:07:29 +0200

    debug build dofmap

--- src/parageom/test_build_dofmap.py
+++ src/parageom/test_build_dofmap.py
@@ -1,17 +1,7 @@
+from mpi4py import MPI
+from dolfinx import fem, mesh
 import numpy as np
-
-
-def blocked(dofs: np.ndarray, bs: int):
-    ndofs = dofs.size
-    blocked = np.zeros((ndofs, bs), dtype=dofs.dtype)
-    for i in range(bs):
-        blocked[:, i] = i
-    r = blocked + np.repeat(dofs[:, np.newaxis], bs, axis=1) * bs
-    return r.flatten()
-
-# Note
-# We have to loop over the full mesh to determine affected cells.
-# Then we can build the submesh and subspace.
+from multi.misc import x_dofs_vectorspace
 
 
 def build_dof_map(V, cell_map, V_r, dofs) -> np.ndarray:
@@ -29,60 +19,87 @@ def build_dof_map(V, cell_map, V_r, dofs) -> np.ndarray:
     tdim = subdomain.topology.dim
     num_cells = subdomain.topology.index_map(tdim).size_local
 
-    parents = []
-    children = []
+    children = set()
 
     for cell in range(num_cells):
         parent_cell = cell_map[cell]
-        parent_dofs = blocked(V.dofmap.cell_dofs(parent_cell), V.dofmap.bs)
-        child_dofs = blocked(V_r.dofmap.cell_dofs(cell), V_r.dofmap.bs)
+        parent_dofs_local = V.dofmap.cell_dofs(parent_cell)
+        child_dofs_local = V_r.dofmap.cell_dofs(cell)
 
-        parents.append(parent_dofs)
-        children.append(child_dofs)
-    parents = np.unique(np.hstack(parents))
-    children = np.unique(np.hstack(children))
-    indx = np.nonzero(parents[:, np.newaxis] - dofs[np.newaxis, :]==0)[0]
-    return children[indx]
+        for b in range(V.dofmap.bs):
+            for pdof, cdof in zip(parent_dofs_local, child_dofs_local):
+                if pdof * V.dofmap.bs + b in dofs:
+                    children.add(cdof * V.dofmap.bs + b)
+    return np.array(list(children), dtype=dofs.dtype)
 
 
-def test():
-    from mpi4py import MPI
-    from dolfinx import fem, mesh, default_scalar_type
-    import ufl
-    domain = mesh.create_unit_interval(MPI.COMM_SELF, 5)
-    V = fem.functionspace(domain, ("P", 1, (2,)))
-    magic_dofs = np.array([4, 5, 10], dtype=np.int32)
+def test(domain, value_shape):
+
+    # TODO
+    # other domain
+    # other value shape
+
+    V = fem.functionspace(domain, ("P", 1, value_shape))
+
+    ndofs = V.dofmap.bs * V.dofmap.index_map.size_local
+    magic_dofs = set()
+    nmagic = 12
+    while len(magic_dofs) < nmagic:
+        magic_dofs.add(np.random.randint(0, ndofs))
+
+    magic_dofs = np.array(list(magic_dofs), dtype=np.int32)
     
     range_dofmap = V.dofmap
     num_cells = domain.topology.index_map(domain.topology.dim).size_local
     affected_cell_indices = set()
     for cell_index in range(num_cells):
         local_dofs = range_dofmap.cell_dofs(cell_index)
-        # dofmap.cell_dofs() returns non-blocked dof indices
-        for ld in blocked(local_dofs, range_dofmap.bs):
-            if ld in magic_dofs:
-                affected_cell_indices.add(cell_index)
-                continue
+        for ld in local_dofs:
+            for b in range(range_dofmap.bs):
+                if ld * range_dofmap.bs + b in magic_dofs:
+                    affected_cell_indices.add(cell_index)
+                    continue
     affected_cell_indices = np.array(list(sorted(affected_cell_indices)), dtype=np.int32)
     
+    source_dofmap = V.dofmap
+    source_dofs = set()
+    for cell_index in affected_cell_indices:
+        ldofs = source_dofmap.cell_dofs(cell_index)
+        for ld in ldofs:
+            for b in range(source_dofmap.bs):
+                source_dofs.add(ld * source_dofmap.bs + b)
+
+    source_dofs = np.array(sorted(source_dofs), dtype=np.int32)
+
     tdim = domain.topology.dim
     submesh, cell_map, _, _ = mesh.create_submesh(domain, tdim, affected_cell_indices)
     Vsub = fem.functionspace(submesh, V.ufl_element())
-    map = build_dof_map(V, cell_map, Vsub, magic_dofs)
 
-    v = ufl.TestFunction(V)
-    c = fem.Constant(domain, (default_scalar_type(2.), default_scalar_type(-4.)))
-    form = ufl.inner(v, c) * ufl.dx
-    vector = fem.assemble_vector(fem.form(form))
+    r_source_dofs = build_dof_map(V, cell_map, Vsub, source_dofs)
+    assert source_dofs.size == r_source_dofs.size
 
-    # restriced evaluation
-    v = ufl.TestFunction(Vsub)
-    c = fem.Constant(Vsub.mesh, (default_scalar_type(2.), default_scalar_type(-4.)))
-    form = ufl.inner(v, c) * ufl.dx
-    other = fem.assemble_vector(fem.form(form))
+    # compare dof coordinates
+    xdofs = x_dofs_vectorspace(V)[source_dofs[np.argsort(r_source_dofs)]]
+    other = x_dofs_vectorspace(Vsub)
 
-    assert np.allclose(vector.array[magic_dofs], other.array[map])
+
+    diff = np.abs(xdofs - other)
+    if domain.topology.dim == 2:
+        breakpoint()
+        # FIXME
+        # this does not work in 2D
+    assert np.sum(diff) < 1e-9
+
 
 
 if __name__ == "__main__":
-    test()
+    num_intervals = 20
+    unit_interval = mesh.create_unit_interval(MPI.COMM_WORLD, num_intervals)
+
+    test(unit_interval, ())
+    test(unit_interval, (2,))
+
+    num_triangles = 8
+    unit_square = mesh.create_unit_square(MPI.COMM_WORLD, num_triangles, num_triangles)
+
+    test(unit_square, ())
