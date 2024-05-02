086b4fe2bfb86b0f08c3ef0fe7eb8e7e8b935feb
Philipp Diercks <philipp.diercks@gmail.com>
2024-05-02 18:07:46 +0200

    improve test regarding form restriction

--- src/parageom/test_form_restriction.py
+++ src/parageom/test_form_restriction.py
@@ -1,9 +1,12 @@
 import numpy as np
+import numpy.typing as npt
+
 from mpi4py import MPI
 from dolfinx import fem, mesh, default_scalar_type
 from dolfinx.fem.petsc import create_matrix, assemble_matrix
 import ufl
 
+import matplotlib.pyplot as plt
 from scipy.sparse import csr_array
 
 
@@ -43,6 +46,15 @@ def replace_integral_domains(form, common_domain):  # TODO: Move elsewhere
     if reconstruct:
         form = ufl.Form(integrals)
     return form
+
+
+def blocked(dofs: npt.NDArray[np.int32], bs: int) -> npt.NDArray[np.int32]:
+    ndofs = dofs.size
+    blocked = np.zeros((ndofs, bs), dtype=dofs.dtype)
+    for i in range(bs):
+        blocked[:, i] = i
+    r = blocked + np.repeat(dofs[:, np.newaxis], bs, axis=1) * bs
+    return r.flatten()
 
 
 def restrict_form(form, S, R, V_r_source, V_r_range, interpolation_data=None):
@@ -127,25 +139,47 @@ def affected_cells(V, dofs):
     dofmap = V.dofmap
 
 
-    affected_cells = []
+    affected_cells = set()
     num_cells = domain.topology.index_map(domain.topology.dim).size_local
     for cell in range(num_cells):
         cell_dofs = dofmap.cell_dofs(cell)
         for dof in cell_dofs:
             for b in range(dofmap.bs):
                 if dof * dofmap.bs + b in dofs:
-                    affected_cells.append(cell)
+                    affected_cells.add(cell)
+                    continue
 
-    affected_cells = np.array(affected_cells, dtype=np.int32)
+    affected_cells = np.array(list(sorted(affected_cells)), dtype=np.int32)
     return affected_cells
+
+def plot_xdofs(V):
+
+    xdofs = V.tabulate_dof_coordinates()
+    gdim = 2
+    xdofs = xdofs[:, :gdim]
+    x, y = xdofs.T
+
+    plt.figure(1)
+    plt.scatter(x, y, facecolors="none", edgecolors="k", marker="o")
+
+    for i, (xx, yy) in enumerate(xdofs):
+        plt.annotate(str(i), (xx, yy))
+
+
 
 
 def test(form_str):
     from .test_build_dofmap import build_dof_map
 
-    num_cells = 20
+    # num_cells = 20
     # domain = mesh.create_unit_interval(MPI.COMM_WORLD, num_cells)
-    domain = mesh.create_unit_square(MPI.COMM_WORLD, 4, 4)
+    # domain = mesh.create_unit_square(MPI.COMM_WORLD, 2, 2)
+    nx = ny = 6
+    width = 1. * nx
+    height = 1. * ny
+    domain = mesh.create_rectangle(MPI.COMM_WORLD, np.array([[0., 0.], [width, height]]), np.array([nx, ny], dtype=np.int32))
+    num_cells = domain.topology.index_map(2).size_local
+    print(f"{num_cells=}")
     V = fem.functionspace(domain, ("P", 1))
     u = ufl.TrialFunction(V)
     v = ufl.TestFunction(V)
@@ -170,10 +204,24 @@ def test(form_str):
     # overhead = source_dofs - magic_dofs 
 
     tdim = domain.topology.dim
-    source_dofs = np.array([3, 6, 7, 11, 15], dtype=np.int32)
-    cells = affected_cells(V, source_dofs)
-    submesh, cell_map, _, _ = mesh.create_submesh(domain, tdim, np.unique(cells))
+    num_idofs = 10
+    num_dofs = V.dofmap.bs * V.dofmap.index_map.size_local
+    idofs = set()
+    while len(idofs) < num_idofs:
+        idofs.add(np.random.randint(0, num_dofs))
+    idofs = np.array(list(sorted(idofs)), dtype=np.int32)
+    print(f"{idofs=}")
+    cells = affected_cells(V, idofs)
+    print(f"affected cells: {cells}")
+    submesh, cell_map, _, _ = mesh.create_submesh(domain, tdim, cells)
     Vsub = fem.functionspace(submesh, V.ufl_element())
+
+    source_dofmap = V.dofmap
+    source_dofs = set()
+    for cell_index in cells:
+        local_dofs = blocked(source_dofmap.cell_dofs(cell_index), source_dofmap.bs)
+        source_dofs.update(local_dofs)
+    source_dofs = np.array(sorted(source_dofs), dtype=idofs.dtype)
 
     interpolation_data = None
     if form_str == "function":
@@ -183,26 +231,43 @@ def test(form_str):
                 V.mesh)
 
     rform, _, _ = restrict_form(mass, V, V, Vsub, Vsub, interpolation_data=interpolation_data)
+
+    # FIXME
+    # build dof map seems to be wrong!!!
+    restricted_source_dofs = build_dof_map(V, cell_map, Vsub, source_dofs)
+    restricted_range_dofs = build_dof_map(V, cell_map, Vsub, idofs)
+
+    breakpoint()
+    # sanity check for dof mappings
+    x_dofs_V = V.tabulate_dof_coordinates()
+    x_dofs_V_r = Vsub.tabulate_dof_coordinates()
+    diff = x_dofs_V[source_dofs] - x_dofs_V_r[restricted_source_dofs]
+    assert np.sum(np.abs(diff)) < 1e-9
+
+    # when are the restricted source dofs needed?
+    # when doing U.dofs(source_dofs) the dofs need to be ordered
+    # as the dofs on the submesh, i.e. the restricted source dofs.
+    # see computation of RG below
+
+    # Be careful when cells/dofs are sorted and when not???
+
     M = assemble_form(mass)
     R = assemble_form(rform)
 
-    matrix = csr_array(M.getValuesCSR()[::-1]).todense()
-    other = csr_array(R.getValuesCSR()[::-1]).todense()
+    g = fem.Function(V)
+    g.x.array[:] = np.random.rand(num_dofs)
 
-    restricted_source_dofs = build_dof_map(V, cell_map, Vsub, source_dofs)
+    matrix = csr_array(M.getValuesCSR()[::-1])
+    reference = matrix.dot(g.x.array[:])[idofs]
+
+    other = csr_array(R.getValuesCSR()[::-1])
+    mask = source_dofs[np.argsort(restricted_source_dofs)]
+    RG = other.dot(g.x.array[mask])
+    result = RG[restricted_range_dofs]
+
     breakpoint()
-    assert np.allclose(matrix[np.ix_(source_dofs, source_dofs)], other[np.ix_(restricted_source_dofs, restricted_source_dofs)])
-    breakpoint()
 
-    # FIXME
-    # the tests passed for the unit interval, but fail for the unit square
-    # for the current configuration using
-    # source_dofs[1:]
-    # restricted_source_dofs[1:]
-    # the test passes
-
-    # make a MWE where you can count dofs, but the test does not pass
-    # might be a problem with `build_dof_map`
+    assert np.allclose(reference, result)
 
 
 
