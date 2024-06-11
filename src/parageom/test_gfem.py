3eff4f078e3cf6fa3f4f73bd93a96f88827aee05
Philipp Diercks <philipp.diercks@gmail.com>
2024-06-11 16:29:33 +0200

    restructure gfem

--- src/parageom/test_gfem.py
+++ src/parageom/test_gfem.py
@@ -1,62 +1,58 @@
 import pathlib
-import tempfile
 from mpi4py import MPI
 import dolfinx as df
 import numpy as np
-from multi.preprocessing import create_rectangle
 from multi.io import read_mesh
 from multi.domain import StructuredQuadGrid
 from multi.misc import x_dofs_vectorspace
-from pymor.bindings.fenicsx import FenicsxVectorSpace, FenicsxVisualizer
+from pymor.bindings.fenicsx import FenicsxVectorSpace
+from pymor.core.logger import getLogger
+from pymor.core.defaults import set_defaults
 
 
-def main():
+def main(args):
     from .tasks import example
-    unit_length = example.unit_length
-    xmin = ymin = 0.
-    xmax = 2 * unit_length
-    ymax = 1 * unit_length
-    gdim = 2
+    gdim = example.gdim
+
+    stem = pathlib.Path(__file__).stem  # gfem
+    logfilename = example.log_gfem(
+        args.nreal, args.method, args.distribution
+    ).as_posix()
+    set_defaults({"pymor.core.logger.getLogger.filename": logfilename})
+    logger = getLogger(stem, level="DEBUG")
 
     # ### coarse grid
     domain, _, _ = read_mesh(example.coarse_grid("global"), MPI.COMM_WORLD, kwargs={"gdim": gdim})
-    # with tempfile.NamedTemporaryFile(suffix=".msh") as tf:
-    #     create_rectangle(xmin, xmax, ymin, ymax, num_cells=(2, 1), recombine=True, out_file=tf.name)
-    #     domain, _, _ = read_mesh(pathlib.Path(tf.name), MPI.COMM_WORLD, kwargs={"gdim": gdim})
 
     # ### fine grid 
-    # unit_cell_msh = example.parent_unit_cell.as_posix()
     struct_grid = StructuredQuadGrid(domain)
-    # struct_grid.fine_grid_method = [unit_cell_msh]
-    # with tempfile.NamedTemporaryFile(suffix=".xdmf") as tf:
-    # struct_grid.create_fine_grid(np.arange(struct_grid.num_cells), "mymesh.xdmf", "quad9")
-    # xdmf written by meshio has `name` 'Grid'
-    # fine_domain = read_mesh(pathlib.Path("mymesh.xdmf"), MPI.COMM_WORLD, kwargs={"name": "Grid"})[0]
-
-    # actually I do not need the fine grid
-    # but only the unit cell domain that is translated
-
-    # is there an easy way to get the node for some dof?
-
-    unit_cell, ct, ft = read_mesh(example.parent_unit_cell, MPI.COMM_WORLD, kwargs={"gdim": gdim})
+    unit_cell, _, _ = read_mesh(example.parent_unit_cell, MPI.COMM_WORLD, kwargs={"gdim": gdim})
     x_unit_cell = unit_cell.geometry.x
     value_shape = (gdim, )
-    V = df.fem.functionspace(unit_cell, ("P", 2, value_shape))
-    basis_left = np.load(example.hapod_modes_npy(0, "normal", "left"))
-    basis_inner = np.load(example.hapod_modes_npy(0, "normal", "inner"))
-    basis_right = np.load(example.hapod_modes_npy(0, "normal", "right"))
+    V = df.fem.functionspace(unit_cell, ("P", example.fe_deg, value_shape))
+
+    distr = args.distribution
+    if args.method == "hapod":
+        basis_left = np.load(example.hapod_modes_npy(args.nreal, distr, "left"))
+        basis_inner = np.load(example.hapod_modes_npy(args.nreal, distr, "inner"))
+        basis_right = np.load(example.hapod_modes_npy(args.nreal, distr, "right"))
+    elif args.method == "heuristic":
+        basis_left = np.load(example.heuristic_modes_npy(args.nreal, distr, "left"))
+        basis_inner = np.load(example.heuristic_modes_npy(args.nreal, distr, "inner"))
+        basis_right = np.load(example.heuristic_modes_npy(args.nreal, distr, "right"))
+    else:
+        raise NotImplementedError
     bases = [basis_left, basis_inner, basis_right]
     bases_length = [len(rb) for rb in bases]
     min_basis_length = min(bases_length)
-    print(f"{min_basis_length=}")
+    logger.debug(f"{min_basis_length=}")
+
     source = FenicsxVectorSpace(V)
     phi = df.fem.Function(V)
-    viz = FenicsxVisualizer(source)
     u = df.fem.Function(V)
 
     W = df.fem.functionspace(domain, ("P", 1, value_shape))
     w = df.fem.Function(W)
-    # num_dofs = W.dofmap.index_map.size_local * W.dofmap.index_map_bs
     xdofs = x_dofs_vectorspace(W)
     xverts = struct_grid.grid.geometry.x
     assert xverts.shape[0] == 22
@@ -72,8 +68,6 @@ def main():
     vertex_to_owning_cell[[0, 2]] = 0 # left boundary
     vertex_to_owning_cell[[20, 21]] = 9 # left boundary
 
-    breakpoint()
-
     w_vec = w.x.petsc_vec # hat function in coarse space
     u_vec = u.x.petsc_vec # store basis functions
     phi_vec = phi.x.petsc_vec # store hat function
@@ -83,42 +77,35 @@ def main():
     for cell in range(struct_grid.num_cells):
         num_gfem_dofs = 0
         gfem = []
-        hats = []
+        # hats = []
 
         vertices = struct_grid.get_entities(0, cell)
         x_vertices = struct_grid.get_entity_coordinates(0, vertices)
         dx = x_vertices[0]
-        dx = np.around(dx, decimals=2)
+        dx = np.around(dx, decimals=3)
         x_unit_cell += dx
 
-        print(f"{cell=}")
-        print(f"{dx=}")
-        print(f"{vertices=}")
+        logger.debug(f"{cell=}")
+        logger.debug(f"{dx=}")
+        logger.debug(f"{vertices=}")
 
         # compute interpolation data after unit cell domain was translated!
         interp_data = df.fem.create_nonmatching_meshes_interpolation_data(
-                V.mesh, V.element, W.mesh, padding=1e-14)
+                V.mesh, V.element, W.mesh, padding=1e-9)
 
         ldofs = W.dofmap.cell_dofs(cell)
         for ld, vert in zip(ldofs, vertices):
-            # print(f"{vert=}\t {xverts[vert]=}")
             w_vec.zeroEntries()
             for b in range(W.dofmap.index_map_bs):
                 dof = ld * W.dofmap.index_map_bs + b
-                # print(f"{dof=}\t {xdofs[dof]=}")
                 assert np.allclose(xdofs[dof], xverts[vert])
 
-                # w_vec.zeroEntries()
                 w_vec.array[dof] = 1.0
-                # w.x.scatter_forward()
             w.x.scatter_forward()
 
             phi_vec.zeroEntries()
             phi.interpolate(w, nmm_interpolation_data=interp_data)
             phi.x.scatter_forward()
-
-            print("phi max: ", np.amax(phi_vec.array))
-            hats.append(phi_vec.copy())
 
             # FIXME
             # the interpolation of w into phi is not leading to
@@ -128,9 +115,8 @@ def main():
             # this theoretically leads to gaps/overlaps in the
             # global approximation
 
-            # TODO: need to loop over basis set of the archetype
-            print(f"{vert=}")
-            print(f"{vertex_to_basis[vert]=}")
+            logger.debug(f"{vert=}")
+            logger.debug(f"{vertex_to_basis[vert]=}")
             basis = bases[vertex_to_basis[vert]]
             sign = 1.0
             if vertex_to_owning_cell[vert] != cell:
@@ -155,20 +141,21 @@ def main():
                 num_gfem_dofs += 1
 
         assert len(gfem) == num_gfem_dofs
-        assert len(hats) == 4
-        print(f"{cell=}\t{num_gfem_dofs=}")
-        # G = np.vstack(gfem)
-        # GG = source.from_numpy(G)
-        # P = np.vstack(hats)
-        # PHI = source.make_array(hats)
-        GG = source.make_array(gfem)
-        viz.visualize(GG, filename=f"gg_{cell}.xdmf")
-        # viz.visualize(PHI, filename=f"phi_{cell}.xdmf")
-        # breakpoint()
-        # w.x.array[i] = 1.0
-        # interpolate u into subdomain space to get local hat function
-        # also requires to locate the dof and associated node and from the node choose the subdomain
+        logger.debug(f"{cell=}\t{num_gfem_dofs=}")
+
+        G = source.make_array(gfem) # type: ignore
+        outpath = example.local_basis_npy(args.nreal, args.method, distr, cell)
+        np.save(outpath.as_posix(), G.to_numpy())
+
         x_unit_cell -= dx
 
+
 if __name__ == "__main__":
-    main()
+    import sys
+    import argparse
+    parser = argparse.ArgumentParser(description="Construct global approximation via GFEM.")
+    parser.add_argument("nreal", type=int, help="The n-th realization.")
+    parser.add_argument("method", type=str, help="Method that was used to construct local bases.")
+    parser.add_argument("distribution", type=str, help="Distribution used for random sampling.")
+    args = parser.parse_args(sys.argv[1:])
+    main(args)
