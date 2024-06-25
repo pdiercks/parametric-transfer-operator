fix scaling of inner product

src/parageom/definitions.py
@@ -79,9 +79,9 @@ class BeamData:
     epsilon_star: dict = field(
             default_factory=lambda: {
                 "heuristic": 0.1,
-                "hapod": 0.1 / np.sqrt(10),
+                "hapod": 0.1,
                 })
-    omega: float = 0.5
+    omega: float = 0.001
     rrf_ttol: float = 10e-2
     rrf_ftol: float = 1e-10
     rrf_num_testvecs: int = 20

src/parageom/fom.py
@@ -243,24 +243,24 @@ def discretize_fom(example: BeamData, auxiliary_problem, trafo_disp):
     product_l2 = "l2"
     l2_product = FenicsxMatrixOperator(product_mat, V, V, name=product_l2)
 
-    u = ufl.TrialFunction(V)
-    v = ufl.TestFunction(V)
-    l_char = df.fem.Constant(V.mesh, df.default_scalar_type(100. ** 2))
-    scaled_h1_0_semi = l_char * ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
-    a_cpp = df.fem.form(scaled_h1_0_semi)
-    A = dolfinx.fem.petsc.create_matrix(a_cpp)
-    A.zeroEntries()
-    dolfinx.fem.petsc.assemble_matrix(A, a_cpp, bcs=bcs)
-    A.assemble()
-    scaled_h1_product = FenicsxMatrixOperator(A, V, V, name="scaled_h1_0_semi")
+    # u = ufl.TrialFunction(V)
+    # v = ufl.TestFunction(V)
+    # l_char = df.fem.Constant(V.mesh, df.default_scalar_type(100. ** 2))
+    # scaled_h1_0_semi = l_char * ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
+    # a_cpp = df.fem.form(scaled_h1_0_semi)
+    # A = dolfinx.fem.petsc.create_matrix(a_cpp)
+    # A.zeroEntries()
+    # dolfinx.fem.petsc.assemble_matrix(A, a_cpp, bcs=bcs)
+    # A.assemble()
+    # scaled_h1_product = FenicsxMatrixOperator(A, V, V, name="scaled_h1_0_semi")
 
-    scaled_l2 = l_char * ufl.inner(u, v) * ufl.dx
-    l2_cpp = df.fem.form(scaled_l2)
-    P = dolfinx.fem.petsc.create_matrix(l2_cpp)
-    P.zeroEntries()
-    dolfinx.fem.petsc.assemble_matrix(P, l2_cpp, bcs=bcs)
-    P.assemble()
-    scaled_l2_product = FenicsxMatrixOperator(P, V, V, name="scaled_l2")
+    # scaled_l2 = l_char * ufl.inner(u, v) * ufl.dx
+    # l2_cpp = df.fem.form(scaled_l2)
+    # P = dolfinx.fem.petsc.create_matrix(l2_cpp)
+    # P.zeroEntries()
+    # dolfinx.fem.petsc.assemble_matrix(P, l2_cpp, bcs=bcs)
+    # P.assemble()
+    # scaled_l2_product = FenicsxMatrixOperator(P, V, V, name="scaled_l2")
 
     # ### Visualizer
     viz = FenicsxVisualizer(FenicsxVectorSpace(V))
@@ -271,7 +271,7 @@ def discretize_fom(example: BeamData, auxiliary_problem, trafo_disp):
     fom = StationaryModel(
         operator,
         rhs,
-        products={product_name: h1_product, product_l2: l2_product, "scaled_h1_0_semi": scaled_h1_product, "scaled_l2": scaled_l2_product},
+        products={product_name: h1_product, product_l2: l2_product},
         visualizer=viz,
         name="FOM",
     )
@@ -305,23 +305,16 @@ if __name__ == "__main__":
     U = fom.solve(mu) # dimensionless solution U, real displacement D=l_char * U
     # with characteristic length l_char = 100. mm (unit length)
 
-    Unorm = U.norm(fom.scaled_h1_0_semi_product)
-    Unorm_l2 = U.norm(fom.scaled_l2_product)
-
     D = U.copy()
     l_char = 100.
     D.scal(l_char)
-    Dnorm = D.norm(fom.h1_0_semi_product)
-    Dnorm_l2 = D.norm(fom.l2_product)
 
     # check norm of displacement field
     assert np.isclose(D.norm(), U.norm() * l_char)
-    assert np.isclose(Dnorm, Unorm)
-    assert np.isclose(Dnorm_l2, Unorm_l2)
+    assert np.isclose(D.norm(fom.h1_0_semi_product), U.norm(fom.h1_0_semi_product) * l_char)
 
     # check load
     total_load = np.sum(fom.rhs.as_range_array().to_numpy())  # type: ignore
     assert np.isclose(total_load, -example.traction_y / example.youngs_modulus * 10)
 
     fom.visualize(U, filename="fom_mu_bar.xdmf")
-    breakpoint()

src/parageom/hapod.py
@@ -226,7 +226,7 @@ def main(args):
     snapshots = transfer.range.empty()
     spectral_basis_sizes = list()
 
-    epsilon_star = example.epsilon_star["hapod"]
+    epsilon_star = example.epsilon_star["hapod"] / example.l_char
     omega = example.omega
     epsilon_alpha = (
         np.sqrt(example.rrf_num_testvecs) * np.sqrt(1.0 - omega**2.0) * epsilon_star
@@ -238,7 +238,7 @@ def main(args):
             basis = adaptive_rrf_normal(
                 logger,
                 transfer,
-                error_tol=example.rrf_ttol,
+                error_tol=example.rrf_ttol / example.l_char,
                 failure_tolerance=example.rrf_ftol,
                 num_testvecs=example.rrf_num_testvecs,
                 l2_err=epsilon_alpha,

src/parageom/projerr.py
@@ -80,7 +80,7 @@ def main(args):
 
         snapshots = transfer.range.empty()
         spectral_basis_sizes = list()
-        epsilon_star = epsilon_star[args.method]
+        epsilon_star = epsilon_star[args.method] / example.l_char
         epsilon_alpha = np.sqrt(example.rrf_num_testvecs) * np.sqrt(1 - omega**2.) * epsilon_star
 
         for mu, seed_seq in zip(training_set, seed_seqs_rrf):
@@ -89,7 +89,7 @@ def main(args):
                 rb = adaptive_rrf_normal(
                     logger,
                     transfer,
-                    error_tol=example.rrf_ttol,
+                    error_tol=example.rrf_ttol / example.l_char,
                     failure_tolerance=example.rrf_ftol,
                     num_testvecs=example.rrf_num_testvecs,
                     l2_err=epsilon_alpha,
@@ -106,7 +106,7 @@ def main(args):
 
     elif args.method == "heuristic":
         from .heuristic import heuristic_range_finder
-        epsilon_star = epsilon_star[args.method]
+        epsilon_star = epsilon_star[args.method] / example.l_char
 
         with new_rng(seed_seqs_rrf[0]):
             spectral_basis, _ = heuristic_range_finder(
@@ -114,7 +114,7 @@ def main(args):
                 transfer,
                 training_set,
                 testing_set,
-                error_tol=example.rrf_ttol,
+                error_tol=example.rrf_ttol / example.l_char,
                 failure_tolerance=example.rrf_ftol,
                 num_testvecs=example.rrf_num_testvecs,
                 l2_err=epsilon_star,
@@ -156,7 +156,7 @@ def main(args):
     aerrs = []
     rerrs = []
     l2errs = []
-    u_norm = test_data.norm(transfer.range_product)  # norm of each test vector
+    u_norm = example.l_char * test_data.norm(transfer.range_product)  # norm of each test vector
 
     logger.info("Computing projection error ...")
     for N in range(len(basis) + 1):
@@ -167,13 +167,13 @@ def main(args):
             orthonormal=orthonormal,
         )
         err = test_data - U_proj # type: ignore
-        errn = err.norm(transfer.range_product)  # absolute projection error
+        errn = example.l_char * err.norm(transfer.range_product)  # absolute projection error
         if np.all(errn == 0.0):
             # ensure to return 0 here even when the norm of U is zero
             rel_err = errn
         else:
             rel_err = errn / u_norm
-        l2_err = np.sum((err).norm2(transfer.range_product)) / len(test_data)
+        l2_err = np.sum(example.l_char**2. * (err).norm2(transfer.range_product)) / len(test_data)
 
         aerrs.append(np.max(errn))
         rerrs.append(np.max(rel_err))